60b098c52d018b01584c8862b4c238afbd2a8244
RikkaW <rikka@shizuku.moe>
2017-09-10 16:54:00 +0800

    clean up demo / add bad image getter / text size 15sp / line spacing extra 2sp

--- app/src/main/java/com/zzhoujay/markdowndemo/MainActivity.java
+++ app/src/main/java/com/zzhoujay/markdowndemo/MainActivity.java
@@ -1,10 +1,14 @@
 package com.zzhoujay.markdowndemo;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.StrictMode;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
@@ -14,28 +18,19 @@ import android.widget.Toast;
 
 import com.zzhoujay.markdown.MarkDown;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final String test_str = "# MarkDown\n" +
-            "\n" +
-            "> Android平台的Markdown解析器\n" +
-            "\n" +
-            "##### 开发中。。。\n" +
-            "\n" +
-            "__by zzhoujay__\n";
-
-    private static final String markdown_test = "image: ![image](http://image.tianjimedia.com/uploadImages/2015/129/56/J63MI042Z4P8.jpg)\n[link](https://github.com/zzhoujay/RichText/issues)";
-
-    private static final String empty_string = "  \n   \n  \n# hello";
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textView);
+        final TextView textView = findViewById(R.id.textView);
         assert textView != null;
         textView.setMovementMethod(LinkMovementMethod.getInstance());
 
@@ -51,8 +46,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public Drawable getDrawable(String source) {
                         Log.d(TAG, "getDrawable() called with: source = [" + source + "]");
-                        Drawable drawable = new ColorDrawable(Color.LTGRAY);
-                        drawable.setBounds(0, 0, textView.getWidth() - textView.getPaddingLeft() - textView.getPaddingRight(), 400);
+
+                        Drawable drawable;
+                        try {
+                            drawable = drawableFromUrl(source);
+                            drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+                        } catch (IOException e) {
+                            Log.w(TAG, "can't get image", e);
+                            drawable = new ColorDrawable(Color.LTGRAY);
+                            drawable.setBounds(0, 0, textView.getWidth() - textView.getPaddingLeft() - textView.getPaddingRight(), 400);
+                        }
                         return drawable;
                     }
                 }, textView);
@@ -61,6 +64,19 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(spanned);
             }
         });
+    }
 
+    public static Drawable drawableFromUrl(String url) throws IOException {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return new BitmapDrawable(x);
     }
 }
